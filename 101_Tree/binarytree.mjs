bf7ad7e8d3cab9534431bbf2c61c7a605fc7213c
jakke <itsjakk3@gmail.com>
2023-12-20 09:26:36 +0900

    Tree

---
+++ 101_Tree/binarytree.mjs
@@ -0,0 +1,54 @@
+class BinaryTree {
+    constructor(data, leftTree = null, rightTree = null) {
+        this.data = data;
+        this.leftSubTree = leftTree;
+        this.rightSubTree = rightTree;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    setData(data) {
+        this.data = data;
+    }
+
+    getLeftSubTree() {
+        return this.leftSubTree;
+    }
+
+    getRightSubTree() {
+        return this.rightSubTree;
+    }
+
+    setLeftSubTree(tree) {
+        this.leftSubTree = tree;
+    }
+
+    setRightSubTree(tree) {
+        this.rightSubTree = tree;
+    }
+
+    preOrderTraveral(tree) {
+        if(tree == null) return;
+        console.log(tree.data);
+        this.preOrderTraveral(tree.getLeftSubTree());
+        this.preOrderTraveral(tree.getRightSubTree());
+    }
+
+    inOrderTraveral(tree) {
+        if(tree == null) return;
+        this.inOrderTraveral(tree.getLeftSubTree());
+        console.log(tree.data);
+        this.preOrderTraveral(tree.getRightSubTree());
+    }
+
+    postOrderTraveral(tree) {
+        if(tree == null) return;
+        this.postOrderTraveral(tree.getLeftSubTree());
+        this.postOrderTraveral(tree.getRightSubTree());
+        console.log(tree.data);
+    }
+}
+
+export { BinaryTree };
